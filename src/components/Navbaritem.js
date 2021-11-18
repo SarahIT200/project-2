fed7275ec88637757d35dd17128cb24dfa1c9f5a
Abeer5011 <92170094+Abeer5011@users.noreply.github.com>
2021-11-18 09:10:49 +0300

    s

--- src/components/Navbaritem.js
+++ src/components/Navbaritem.js
@@ -1,23 +1,21 @@
 import { Container, Nav, Navbar } from "react-bootstrap"
 import { Link } from "react-router-dom"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faSignInAlt, faUserPlus } from "@fortawesome/free-solid-svg-icons"
 
 function NavbarItem() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="sticky-top">
       <Container>
         <Link to="/" className="navbar-brand">
-          Enjoy
+          anime world
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
             <Link to="/signup" className="nav-link">
-              <FontAwesomeIcon icon={faUserPlus} />
+              Sign Up
             </Link>
             <Link eventKey={2} to="/login" className="nav-link">
-              <FontAwesomeIcon icon={faSignInAlt} />
+              Login
             </Link>
           </Nav>
         </Navbar.Collapse>
---
+++ src/pages/OneAnime.js
@@ -0,0 +1,5 @@
+function OneAnime() {
+  return <>one page</>
+}
+
+export default OneAnime
