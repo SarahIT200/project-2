0761d685dc7d3454b5c23137ab469503007ee815
Abeer5011 <92170094+Abeer5011@users.noreply.github.com>
2021-11-23 14:35:46 +0300

    d

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -51,6 +51,11 @@ function Home() {
           <button className={Styles.btn3}>Anime</button>
         </Link>
       </div>
+
+      <div className={Styles.text}>
+        <h4>Watch anytime, anywhere</h4>
+        <p>Stream on desktop and mobile. You can add notes in your profile. </p>
+      </div>
     </>
   )
 }
